f3f0ed837ed052ca763db1ceee73398732b907b1
rfciulei <robertciulei97@yahoo.com>
2023-01-08 17:02:42 +0200

    remove useless log function

--- src/main.js
+++ src/main.js
@@ -121,7 +121,6 @@ const createOrEmptyPuzzlesDirSync = () => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -129,75 +128,9 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, also on MacOS.
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+  app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-const log = (string) => {
-  const months = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC",
-  ];
-
-  let current_datetime = new Date();
-  let formatted_date =
-    current_datetime.getHours() + "h-" + current_datetime.getMinutes() + "m";
-
-  let logsFolder = path.join(__dirname, "logs");
-  console.log();
-  if (!fs.existsSync(logsFolder)) {
-    fs.mkdir(logsFolder, (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log("dir was created");
-    });
-  }
-
-  let fileName =
-    "logs_" +
-    current_datetime.getHours() +
-    "h-" +
-    current_datetime.getMinutes() +
-    "m";
-  let currentLogFile = path.join(logsFolder, fileName);
-
-  formatted_date =
-    current_datetime.getHours() +
-    "h-" +
-    current_datetime.getMinutes() +
-    "m-" +
-    current_datetime.getSeconds() +
-    "s-" +
-    current_datetime.getMilliseconds() +
-    "ms";
-
-  fs.writeFileSync(
-    currentLogFile,
-    formatted_date + " [LOG]: " + string,
-    function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(data);
-    }
-  );
-};
-
 
 console.log(addon.hello())
